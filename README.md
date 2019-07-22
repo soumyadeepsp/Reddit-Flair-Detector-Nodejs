ed65c725556f2122ad70883a9b2fc07fe2dcdfa4
soumyadeepsp <52887029+soumyadeepsp@users.noreply.github.com>
2019-07-22 09:13:09 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# Reddit-Flair-Detector-Nodejs
-It predicts the flair of a reddit post from the 'r/india' subreddit flair using machine learning algorithms.
+# Reddit Flair Detector
+
+A Reddit Flair Detector web application to detect flairs of India subreddit posts using Machine Learning algorithms. The application can be found live at [Reddit Flair Detector](https://reddit-flair-detector-sp.herokuapp.com/).
+
+#Url as feature
+accuracy from Naive Bayes = 0.6410256410256411
+accuracy from Linear SVM = 0.5384615384615384
+accuracy from Logistic Regression = 0.48717948717948717
+
+#Title as feature
+accuracy from Naive Bayes = 0.6666666666666666
+accuracy from Linear SVM = 0.6410256410256411
+accuracy from Logistic Regression = 0.5641025641025641
+
+#Title+Url as feature
+accuracy from Naive Bayes = 0.6410256410256411
+accuracy from Linear SVM = 0.5897435897435898
+accuracy from Logistic Regression = 0.5346587647883683
+
+#url_feature.py = this python code uses url as its only feature for prediction
+#title_feature.py = this python code uses title as its only feature for prediction
+#title+url_feature.py = this python code uses url as well as title as its feature for prediction
+
+#precog.html, precog.css, statistics.html, statistics.css = these 4 are the files which are used to desing the website
+
+#reddit-flair-detector-sp = this is the webapp which is hosted on heroku
+
+#data.txt = this file contains the data used for predictions
+
+#statistics.py = this python code is used for the calculations of the statistics.html page which tells te percentage of num_comments and upvote_ratio of differet types of flaired posts.
+
+### References
+
+1. [How to scrape data from Reddit](http://www.storybench.org/how-to-scrape-reddit-with-python/)
+2. [Multi-Class Text Classification Model Comparison and Selection](https://towardsdatascience.com/multi-class-text-classification-model-comparison-and-selection-5eb066197568)
---
+++ statistics.py
@@ -0,0 +1,83 @@
+#! usr/bin/env python3
+import praw
+import copy
+import datetime as dt
+import logging
+import pandas as pd
+import numpy as np
+from numpy import random
+import gensim
+import nltk
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.metrics import accuracy_score, confusion_matrix
+#import matplotlib.pyplot as plt
+from nltk.corpus import stopwords
+import re
+from bs4 import BeautifulSoup
+#%matplotlib inline
+
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfTransformer
+
+#link = input()
+reddit = praw.Reddit(client_id='6Cm46yZ7j1NZSQ', \
+                     client_secret='feB_wRJDyRjL62W4HGSsQTU32Lk', \
+                     user_agent='precog', \
+                     username='sp_paul', \
+                     password='paulsoumitsi')
+#submission = reddit.submission(url=link)
+#print (submission.link_flair_text)
+#title = submission.title
+#flair = submission.link_flair_text
+#print (flair)
+arrc = []
+arru = []
+arrf = []
+subreddit = reddit.subreddit('india')
+top_subreddit = subreddit.top(limit=400)
+for i in top_subreddit:
+	#print (submission.comments[0].body)
+	arrc.append(i.num_comments)
+	arru.append(i.upvote_ratio)
+	arrf.append(i.link_flair_text)
+
+for i in range(0, 400):
+	if (arrf[i]=="AskIndia"):
+		AskIndia_comments += arrc[i]
+		AskIndia_upvotes += arru[i]
+	if (arrf[i]=="Buisness/Finance"):
+		BuisnessFinance_comments += arrc[i]
+		BuisnessFinance_upvotes += arru[i]
+	if (arrf[i]=="Food"):
+		Food_comments += arrc[i]
+		Food_upvotes += arru[i]
+	if (arrf[i]=="Non-Political"):
+		Non-Political_comments += arrc[i]
+		Non-Political_upvotes += arru[i]
+	if (arrf[i]=="Photography"):
+		Photography_comments += arrc[i]
+		Photography_upvotes += arru[i]
+	if (arrf[i]=="Policy/Economy"):
+		Policy_Economy_comments += arrc[i]
+		Policy_Economy_upvotes += arru[i]
+	if (arrf[i]=="Politics"):
+		Politics_comments += arrc[i]
+		Politics_upvotes += arru[i]
+	if (arrf[i]=="Science/Technology"):
+		Science_Technology_comments += arrc[i]
+		Science_Technology_upvotes += arru[i]
+	if (arrf[i]=="Sports"):
+		Sports_comments += arrc[i]
+		Sports_upvotes += arru[i]
+	if (arrf[i]=="[R]eddiquette"):
+		[R]eddiquette_comments += arrc[i]
+		[R]eddiquette_upvotes += arru[i]
+
+	print (AskIndia_comments/400, BuisnessFinance_comments/400, Food_comments/400, Non-Political_comments/400, Photography_comments/400,
+		Policy_Economy_comments/400, Politics_comments/400, Science_Technology_comments/400, Sports_comments/400, [R]eddiquette_comments/400)
+	print (AskIndia_upvotes/400, BuisnessFinance_upvotes/400, Food_upvotes/400, Non-Political_upvotes/400, Photography_upvotes/400,
+		Policy_Economy_upvotes/400, Politics_upvotes/400, Science_Technology_upvotes/400, Sports_upvotes/400, [R]eddiquette_upvotes/400)
+#print (arrc)
+#print (arru)
